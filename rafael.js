2981c549d18ddafba9cab6c62a1a78488154318b
NorbertoRafael <rafflog@hotmail.com>
2018-11-11 12:14:15 -0500

    Delete rafael.js

--- rafael.js
+++
@@ -1,8 +0,0 @@
-var edad = 5;
-var nombre = 'rafael';
-
-var persona = {primerNombre:'rafael', edad: 25,};
-
-console.log('edad', typeof edad)
-console.log('nombre', typeof nombre)
-console.log('persona', typeof persona)
