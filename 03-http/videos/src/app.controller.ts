ab66a7cc5728ca1088a73ffb34fd9c659930bd55
Norberto Andranfo <rafflog@hotmail.com>
2018-11-14 15:49:33 -0500

    Decoradores

--- 03-http/videos/src/app.controller.ts
+++ 03-http/videos/src/app.controller.ts
@@ -1,12 +1,64 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller, Query, Param, Headers} from '@nestjs/common';
 import { AppService } from './app.service';
+import {Observable, ObservableInput, of} from "rxjs";
 
-@Controller()
+@Controller('Usuario')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  //constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
+  @Get('saludar')
+  //@Put, @Post
+  saludar(): string {
+    return 'Hola mundo';
   }
+
+    @Get('despedirse')
+    //@Put, @Post
+    despedirse(): Promise <string> {
+      return new Promise<string>((resolve, reject)=> {
+          resolve('Adios!');
+      })
+
+    };
+
+    @Get('saludarObservable')
+    //@Put, @Post
+    saludarObservable(): Observable <string> {
+        return of('Hola mundo');
+
+    };
+
+    @Get('saludarP')
+    //@Put, @Post
+    saludarP(
+        @Query() queryParams,
+        @Query('nombre') nombre,
+    ): string {
+        return queryParams;
+        //return nombre;
+
+    };
+
+    @Get('segmentoUno/:idUsuario/segmentoDos/')
+    //@Put, @Post
+    ruta(
+        @Param() todosParametrosRuta,
+        @Param('idUsuario') idUsuario,
+    ): string {
+        return idUsuario;
+        //return nombre;
+    };
+
+    @Get('saludo')
+    //@Put, @Post
+    saludo(
+        @Param() todosParametrosRuta,
+        @Param('idUsuario') idUsuario,
+        @Headers('seguridad') seguridad
+    ): string {
+        return idUsuario;
+        //return nombre;
+    };
 }
+
+
